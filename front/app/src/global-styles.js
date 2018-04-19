5181ee4285ca2aed01dd46f7da412e4f345b88e4
Berto Yáñez <berto@ber.to>
2018-04-19 09:28:13 -0500

    updated global styles

--- front/app/src/global-styles.js
+++ front/app/src/global-styles.js
@@ -1,32 +1,18 @@
-import { css } from 'glamor'
+import { css } from 'glamor';
 
-css.insert(`@import url("https://fonts.googleapis.com/css?family=Lato:300,400,700");
-@import "https://api.tiles.mapbox.com/mapbox-gl-js/v0.41.0/mapbox-gl.css";
+const globalStyles = [
+  '@import url("https://fonts.googleapis.com/css?family=Lato:300,400,700");',
+  '@import "https://api.tiles.mapbox.com/mapbox-gl-js/v0.41.0/mapbox-gl.css";',
+  '* { cursor: url("/media/i_ouija.png") 2 2, pointer; }',
+  'html body { padding: 0; margin: 0; font-family: "Lato", sans-serif; }',
+  'a { text-decoration: none; }',
+  '.simpleContainer .mapboxgl-popup-tip { display: none; }',
+  '.simpleContainer .mapboxgl-popup-content { background: rgba(255,255,255, 0.5); padding: 10px 10px 5px; }'
+]
 
-* {
-  cursor: url("/media/i_ouija.png") 2 2, pointer;
+for(const rule of globalStyles) {
+  css.insert(rule);
 }
-
-html,
-body {
-  padding: 0;
-  margin: 0;
-  font-family: "Lato", sans-serif;
-}
-
-a {
-  text-decoration: none;
-}
-
-.simpleContainer .mapboxgl-popup-tip {
-  display: none;
-}
-
-.simpleContainer .mapboxgl-popup-content {
-background: rgba(255,255,255, 0.5);
-padding: 10px 10px 5px;
-}
-`)
 
 export const colors = {
   orange: '#FF9800',
